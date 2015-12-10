a70bd29d2b2f41460dd1e7656e4cff45c130770d
Noah Fichter <nfichter@stuy.edu>
2015-12-10 13:09:18 -0500

    Implemented Volume interface in Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,9 +1,13 @@
-public class Sphere extends Circle{
+public class Sphere extends Circle implements Volume {
     public Sphere(double radius, String name){
 	super(radius,name);
     }
 
+    public double getVolume(){
+	return 4/3*Math.PI*Math.pow(getRadius(),3);
+    }
+
     public String toString(){
-	return "FIXME!";
+	return "Sphere "+getName()+" with radius of "+getRadius();
     }
 }
